a59c05241ddfe0fb7cfd4fc2ad0483b1d19daccc
Florian Willich <florian.willich@student.htw-berlin.de>
2014-11-03 21:22:16 +0100

    nah not working yet...

--- twitter-analytics-tool/src/main/scala/tat/App.scala
+++ twitter-analytics-tool/src/main/scala/tat/App.scala
@@ -1,10 +1,9 @@
 package tat
 
 /* SimpleApp.scala */
-//import org.apache.spark.SparkContext
-//import org.apache.spark.SparkContext._
-//import org.apache.spark.SparkConf
-import org.apache.spark._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
 
 /**
  * @author ${user.name}
@@ -12,15 +11,40 @@ import org.apache.spark._
 object App {
   
 	def main(args: Array[String]) {
-    	
-		val conf = new SparkConf().setAppName("Simple Application")
-		val sc = new SparkContext(conf)
 
-    	val data = sc.textFile(args(0), 2).cache()
+		val sc: SparkContext // An existing SparkContext.
+		val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-    	val a = data.filter(line => line.contains("Spark")).count()
-		
-		println("Text file count: " + a)
+		// createSchemaRDD is used to implicitly convert an RDD to a SchemaRDD.
+		//import sqlContext.createSchemaRDD
+
+		// A JSON dataset is pointed to by path.
+		// The path can be either a single text file or a directory storing text files.
+		val path = "/home/flo/htw/TAT_rep/data.json"
+
+		// Create a SchemaRDD from the file(s) pointed to by path
+		val jsonTweets = sqlContext.jsonFile(path)
+
+		// The inferred schema can be visualized using the printSchema() method.
+		//people.printSchema()
+		// root
+		//  |-- age: IntegerType
+		//  |-- name: StringType
+
+		// Register this SchemaRDD as a table.
+		jsonTweets.registerAsTable("jsonTweets")
+
+		// SQL statements can be run by using the sql methods provided by sqlContext.
+		val hashtags = sqlContext.sql("SELECT entities.hashtags FROM jsonTweets")
+
+		// Alternatively, a SchemaRDD can be created for a JSON dataset represented by
+		// an RDD[String] storing one JSON object per string.
+		val anotherPeopleRDD = sc.parallelize(
+		  """{"name":"Yin","address":{"city":"Columbus","state":"Ohio"}}""" :: Nil)
+		val anotherPeople = sqlContext.jsonRDD(anotherPeopleRDD)
+
+
+
 
   	}
 
