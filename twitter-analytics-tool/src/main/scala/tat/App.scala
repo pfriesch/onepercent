629fab6592a8c3b5678364e70d21419bedab1245
Florian Willich <florian.willich@student.htw-berlin.de>
2014-11-03 15:47:03 +0100

    running

--- twitter-analytics-tool/src/main/scala/tat/App.scala
+++ twitter-analytics-tool/src/main/scala/tat/App.scala
@@ -1,9 +1,10 @@
 package tat
 
 /* SimpleApp.scala */
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
+//import org.apache.spark.SparkContext
+//import org.apache.spark.SparkContext._
+//import org.apache.spark.SparkConf
+import org.apache.spark._
 
 /**
  * @author ${user.name}
@@ -15,13 +16,11 @@ object App {
 		val conf = new SparkConf().setAppName("Simple Application")
 		val sc = new SparkContext(conf)
 
-    	val data = sc.textFile("/opt/spark/README.md", 2).cache()
+    	val data = sc.textFile(args(0), 2).cache()
 
-    	//val a = data.filter(line => line.contains("Spark")).count()
-
-
-    	//val count = textFile.count()
-		//println("Text file count: " + count)
+    	val a = data.filter(line => line.contains("Spark")).count()
+		
+		println("Text file count: " + a)
 
   	}
 
