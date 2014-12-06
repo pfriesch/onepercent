0d32f82e28e960b4e66dcab52cb3e82e7e375aca
pFriesch <p.friesch@posteo.de>
2014-12-06 19:57:42 +0100

    modified TestClient

--- SparkServer/src/test/scala/TestClient.scala
+++ SparkServer/src/test/scala/TestClient.scala
@@ -1,29 +1,29 @@
-package tat.SparkListener
 
 import java.io._
 import java.net.Socket
-import tat.SparkListener.utils._
+
 
 /**
  * Created by plinux on 17/11/14.
  * params:
  *
- * "0000-00-00 00:00:00"  "/home/plinux/workspace/Twitter-Analytics-Tool/sample" "10"
+ * "2014-11-20 05:00:00"  "/studenten/s0540031/tweets/" "10"
  *
  *
  * */
 object TestClient {
 
   def main (args: Array[String]) {
-    println("hostname: " + Config.get.hostname)
-    println("ConfigFilePath: " + Config.getAbsolutConfigFilePath)
-    val socket = new Socket(Config.get.hostname,Config.get.port)
+
+    val socket = new Socket("hadoop03.f4.htw-berlin.de",5555)
+    println("test1")
     val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
+    println("test2")
     val out = new PrintWriter(socket.getOutputStream, true)
 
 //    out.println("{\"job\": \"hashtagtop10\", \"params\":[\"Value1\",\"Value2\"], \"time\":\"0000-00-00 00:00:00\", \"ip\":\"<host-name>\", \"port\":5555}")
     //out.println("{\"job\": \"realTopOfThePops\", \"params\":[\"" + args(0) + "\",\"" + args(1) + "\"], \"time\":\"0000-00-00 00:00:00\", \"ip\":\"<host-name>\", \"port\":5555}")
-
+    println("test3")
     println("gesendet: {\"jobID\":\"superJobID123\",\"name\":\"TopHashtagJob\",\"params\":[\""+args(0)+"\",\""+args(1)+"\",\""+args(2)+"\"],\"time\":\"0000-00-00 00:00:00\"}")
     out.println("{\"jobID\":\"superJobID123\",\"name\":\"TopHashtagJob\",\"params\":[\""+args(0)+"\",\""+args(1)+"\",\""+args(2)+"\"],\"time\":\"0000-00-00 00:00:00\"}")
 
