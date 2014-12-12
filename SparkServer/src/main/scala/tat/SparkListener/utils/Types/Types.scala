f59def11feb9b45b34deab0b9b482b09fe6f54e0
Florian Willich <florian.willich@student.htw-berlin.de>
2014-12-12 18:26:49 +0100

    implemented createMultipleT_Path for getting a list of path between two dates

--- SparkServer/src/main/scala/tat/SparkListener/utils/Types/Types.scala
+++ SparkServer/src/main/scala/tat/SparkListener/utils/Types/Types.scala
@@ -1,6 +1,8 @@
 package tat.SparkListener.utils
 
 //Scala imports
+
+import scala.collection.mutable.ListBuffer
 import scala.util.Try;
 
 //JAVA imports
@@ -47,44 +49,76 @@ object TypeCreator {
 
   /**
    * This method creates a path out of the given parameters as follows:
-   * First the prefixpath within the last / concatenated with the given time
-   * 2014-12-04 14:00:00 will result to <prefixPath>/2014/12/4/14/ this means the hour
+   * First the "/prefix/path/" concatenated with the given time e.g.
+   * "2014-12-04 14:00:00 will" result to /prefix/path/2014/12/4/14/ this means the hour
    * is the last element in the path - finalized with the dataName ending.
    *
    * @param prefixPath      The path to put before the time.
    * @param time            The time with which the path will be build.
-   * @param dataName        THe data/file.
+   * @param dataName        THe data/file e.g. "example.type" or "*.type"
    * @return                A Path if successful.
    *
-   * @author                Florian Willich, Puis Friesch
+   * @author                Florian Willich
    */
   def createT_Path(prefixPath: String, time: GregorianCalendar, dataName: String): Try[T_Path] = {
-    Try(T_Path(prefixPath + time.get(Calendar.YEAR) + "/" + String.format("%02d", time.get(Calendar.MONTH)+1: Integer) + "/" + String.format("%02d", time.get(Calendar.DAY_OF_MONTH): Integer) + "/" + String.format("%02d", time.get(Calendar.HOUR_OF_DAY): Integer) + "/" + dataName))
+    Try(T_Path(prefixPath + createTimePath(time) + dataName))
   }
 
-  def createT_Path(prefixPath: String, timeBegin: GregorianCalendar, timeEnd: GregorianCalendar, dataName: String): Try[T_Path] = {
+  /**
+   * Testing:
+   * val begin: GregorianCalendar = new GregorianCalendar()
+   * begin.setTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2014-12-04 12:00:00"))
+   * val end: GregorianCalendar = new GregorianCalendar()
+   * end.setTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2014-12-04 23:00:00"))
+   * val paths = createT_Path("/prefix/path/", begin, end, "*.data")
+   *
+   * @param     prefixPath                    The path to put before the time.
+   * @param     timeBegin                     The time with which this method will begin to build the Paths.
+   * @param     timeEnd                       The time with which this method will end to build Paths.
+   * @param     dataName                      The data/file.
+   * @throws    IllegalArgumentException      If the date does not differ with a minimum of one (1)
+   *                                          hour.
+   * @return                                  A list of paths.
+   *
+   * @author                Florian Willich
+   */
+  def createMultipleT_Path(prefixPath: String, timeBegin: GregorianCalendar, timeEnd: GregorianCalendar, dataName: String): List[T_Path] = {
     //calculate hours between the dates
-    val hours: Long = (timeEnd.getTimeInMillis - timeBegin.getTimeInMillis) / 3600000
+    //TODO: Maybe without any cast???
+    val hours: Int = ((timeEnd.getTimeInMillis - timeBegin.getTimeInMillis) / 3600000).toInt
 
-    //TODO: Problem when the user wants to much data
-    if (hours > 24 * 7) {
-      throw new Exception()
+    hours match {
+
+      case toLess if toLess < 1 =>
+        throw new IllegalArgumentException("The difference of the dates has to be 1 hour or bigger. Your dates differ the follows: " + toLess)
+
+      case hours =>
+        var pathList: ListBuffer[T_Path] = new ListBuffer[T_Path]()
+        pathList += T_Path(prefixPath + createTimePath(timeBegin) + dataName)
+
+        for (i <- 1 to hours) {
+          timeBegin.add(Calendar.HOUR, 1)
+          pathList += T_Path(prefixPath + createTimePath(timeBegin) + dataName)
+        }
+
+        pathList.toList
+
     }
-
-    val pathList: List[T_Path] = List()
-
-    for (i <- 0 to hours) {
-
-      //TODO: Make path list out of timeBegin to timeEnd like
-      //T_Path(prefixPath, TimeString, data
-      var
-    }
-
 
   }
 
+  /**
+   * This method creates a valid path out of the given time as follows:
+   * "2014-12-04 14:00:00" => "/2014/12/04/14/"
+   *
+   * @param time    The time with which this method will build the path string.
+   *
+   * @return
+   *
+   * @author        Florian Willich, Pius Friesch
+   */
   def createTimePath(time: GregorianCalendar) : String = {
-    time.get(Calendar.YEAR) + "/" + String.format("%02d", time.get(Calendar.MONTH)+1: Integer) + "/" + String.format("%02d", time.get(Calendar.DAY_OF_MONTH): Integer) + "/" + String.format("%02d", time.get(Calendar.HOUR_OF_DAY): Integer)
+    time.get(Calendar.YEAR) + "/" + String.format("%02d", time.get(Calendar.MONTH)+1: Integer) + "/" + String.format("%02d", time.get(Calendar.DAY_OF_MONTH): Integer) + "/" + String.format("%02d", time.get(Calendar.HOUR_OF_DAY): Integer) + "/"
   }
 
 }
