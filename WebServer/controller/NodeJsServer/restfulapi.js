9cd5424cf9f90999d7f7b4732a18a288fd904c74
Patrick M <s0540031@htw-berlin.de>
2014-12-21 16:58:12 +0100

    reworked REST API

--- WebServer/controller/NodeJsServer/restfulapi.js
+++ WebServer/controller/NodeJsServer/restfulapi.js
@@ -6,6 +6,7 @@
 
 var mysql = require('mysql'); // MySQL
 var express = require('express'); // Restapi
+var moment = require('moment'); //Timestampparser
 
 var config = require('./config.js'); //Configurationfile
 var dataBaseHandler = require('./sqlDatabase.js'); //DatabaseHandler
@@ -20,104 +21,40 @@ var server = app.listen(config.browserClientPort, function(){
   console.log("Server is up and running");
 });
 
-/* Database configuration */
-var connection = mysql.createPool (
-  {
-    host     : config.sqlDatabaseHost,
-    port     : config.sqlDatabasePort,
-    user     : config.sqlDatabaseUser,
-    password : config.sqlDatabasePassword,
-    database : config.sqlDatabase,
-    timezone : config.sqlDatabaseTimezone
-  }
-);
-
 /*
  * If the browserClient requests with URLparams (/api/hourly/:table) 
  * this methode gets the needed data from database and responds it to the Browserclient.
  */
 app.get('/api/hourly/:table', function(req, res){
-
-  var query = "SELECT timestamp FROM " + req.params.table +" GROUP BY timestamp";
-  
-  connection.query(query, function(err, rows){
-    if(err) throw err;
-
-    res.send(rows);
+  dataBaseHandler.select("SELECT timestamp FROM ?? GROUP BY timestamp", [req.params.table], function(result){
+    res.send(result);
   });
 });
-/*
-app.get('/api/hourly/:table', function(req, res){
-    dataBaseHandler.readTableFromDatabase(function(response){
-    console.log('table');
-    console.log(response);
-    res.send(response);
-  }, req);
-});
-*/
+
 
 /*
  * If the browserClient requests with URLparams (/api/hourly/:table/:date)
  * this methode gets the needed data from database and responds it to the Browserclient.
  */
 app.get('/api/hourly/:table/:date', function(req, res){
-
   var date = createSQLDate(req.params.date, 00);
   var nextDate = createSQLDate(req.params.date, 00, +24);
-
-  var query = "SELECT * FROM " + req.params.table +" WHERE timestamp >= '" + date + "' AND timestamp < '" + nextDate + "'";
-  
-  connection.query(query, function(err, rows){
-    if(err) throw err;
-
-    res.send(rows);
+  dataBaseHandler.select("SELECT * FROM ?? WHERE timestamp >= ? AND timestamp < ?", [req.params.table,date,nextDate], function(result){
+    res.send(result);
   });
 });
-/*
-app.get('/api/hourly/:table/:date', function(req, res){
-
-  var date = createSQLDate(req.params.date, 00);
-  var nextDate = createSQLDate(req.params.date, 00, +24);
-
-  dataBaseHandler.readTableAndDateFromDatabase(function(response){
-    console.log('tabledate');
-    console.log(response);
-    res.send(response);
-  }, req, date, nextDate);
-});
-*/
 
 /*
  * If the browserClient requests with URLparams (/api/hourly/:table/:date/:hour) 
  * this methode gets the needed data from database and responds it to the Browserclient.
  */
 app.get('/api/hourly/:table/:date/:hour', function(req, res){
-
   var date = createSQLDate(req.params.date, req.params.hour);
   var nextDate = createSQLDate(req.params.date, req.params.hour, +1);
-
-  var query = "SELECT * FROM " + req.params.table +" WHERE timestamp >= '" + date + "' AND timestamp < '" + nextDate + "' ORDER BY count DESC";
-  
-  connection.query(query, function(err, rows){
-    if(err) throw err;
-
-    res.send(rows);
+  dataBaseHandler.select("SELECT * FROM ?? WHERE timestamp >= ? AND timestamp < ? ORDER BY count DESC", [req.params.table,date,nextDate], function(result){
+    res.send(result);
   });
 });
-
-/*
-app.get('/api/hourly/:table/:date/:hour', function(req, res){
-
-  var date = createSQLDate(req.params.date, req.params.hour);
-  var nextDate = createSQLDate(req.params.date, req.params.hour, +1);
-
-  databaseHandler.readTableAndDateAndHourFromDatabase(function(response){
-    console.log('tabledatehour');
-    console.log(response);
-    res.send(response);
-  }, req, date, nextDate); 
-});
-*/
 
 /*
  * Creates a sql timestamp to write the data to database.
@@ -126,39 +63,8 @@ function createSQLDate(date, hour, offset){
   hour = typeof hour !== 'undefined' ? hour : 0;
   offset = typeof offset !== 'undefined' ? offset : 0;
 
-  var newHour = ('00' + hour).slice(-2);
-  var input = date + " " + newHour + ":00:00 UTC";
-
-  var sqldate = new Date(input);
-  sqldate.setHours(sqldate.getHours() + offset);
-
-  sqldate = sqldate.getUTCFullYear() + '-' +
-            ('00' + (sqldate.getUTCMonth() + 1)).slice(-2) + '-' +
-            ('00' + sqldate.getUTCDate()).slice(-2) + ' ' +
-            ('00' + sqldate.getUTCHours()).slice(-2) + ':' +
-            ('00' + sqldate.getUTCMinutes()).slice(-2) + ':' +
-            ('00' + sqldate.getUTCSeconds()).slice(-2);
-
-    return sqldate;
+  var time = moment(date);
+  time.hour(hour);
+  time.add(offset, 'hours');
+  return time.format('YYYY-MM-DD HH:mm:ss');
 }
-
-/*
-app.get('/api/hourly/hash/:numberOfTags', function(req, res){
-
-  console.log("reqParam: " + req.params.numberOfTags);
-
-  dataBaseHandler.readHashtagsFromDatabase(function(response){
-    res.send(response);
-  });
-});
-
-app.get('/api/hourly/time/', function(req, res){
-
-  console.log("reqParam: " + req.params.numberOfTags);
-
-  dataBaseHandler.readHashtagsFromDatabase(function(response){
-    res.send(response);
-  });
-});
-
-*/
