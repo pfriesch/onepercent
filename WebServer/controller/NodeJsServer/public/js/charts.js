e43072f6492d44b043da1c8aab49260a3ed2c58f
Patrick M <s0540031@htw-berlin.de>
2014-12-28 21:00:37 +0100

    move view into controller public

--- WebServer/controller/NodeJsServer/public/js/charts.js
+++ WebServer/controller/NodeJsServer/public/js/charts.js
@@ -1,15 +1,18 @@
 google.load("visualization", "1", {packages:["corechart"]});
 //google.setOnLoadCallback(drawChart);
 
+
+/////////////////////////draw columnchart/////////////////////////////////////
+
 function drawColumnChart(names, values, ytitle, xtitle ) {
 
 	for(i = 0; i < values.length; i++) {
 		values[i] = parseInt(values[i]);
 	}
-	
+
 	names.unshift('');
 	values.unshift('');
-		
+
 	var data = google.visualization.arrayToDataTable([
 		names,
 		values
@@ -19,7 +22,119 @@ function drawColumnChart(names, values, ytitle, xtitle ) {
 		vAxis: {title: ytitle},
 		hAxis: {title: xtitle}
 	};
-	
+
 	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
 	chart.draw(data, options);
+}
+
+
+/////////////////////////draw piechart/////////////////////////////////////
+
+function drawPieChart(names, values) {
+
+	for(i = 0; i < values.length; i++) {
+		values[i] = parseInt(values[i]);
+	}
+
+	names.unshift('');
+	values.unshift('');
+
+	var data = google.visualization.arrayToDataTable([
+		names,
+		values
+	]);
+
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+}
+
+
+/////////////////////////draw donutchart/////////////////////////////////////
+
+function drawDonutChart(names, values) {
+
+	for(i = 0; i < values.length; i++) {
+		values[i] = parseInt(values[i]);
+	}
+
+	names.unshift('');
+	values.unshift('');
+
+	var data = google.visualization.arrayToDataTable([
+		names,
+		values
+	]);
+
+	var options = {
+		pieHole: 0.4,
+	};
+
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+}
+
+
+/////////////////////////draw diffcolumnchart/////////////////////////////////////
+
+function drawDiffColumnChart(names, diffnames, values, diffvalues, ytitle, xtitle ) {
+
+	for(i = 0; i < values.length; i++) {
+		values[i] = parseInt(values[i]);
+	}
+
+	names.unshift('');
+	values.unshift('');
+
+	for(i = 0; i < diffvalues.length; i++) {
+		diffvalues[i] = parseInt(diffvalues[i]);
+	}
+
+	diffnames.unshift('');
+	diffvalues.unshift('');
+
+	var olddata = google.visualization.arrayToDataTable([
+		names,
+		values
+	]);
+
+	var newdata = google.visualization.arrayToDataTable([
+		diffnames,
+		diffvalues
+	]);
+
+	var options = {
+		vAxis: {title: ytitle},
+		hAxis: {title: xtitle}
+	};
+
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	var diffData = chart.computeDiff(oldData, newData);
+	chart.draw(diffData, options);
+}
+
+/////////////////////////draw linechart/////////////////////////////////////
+
+function drawLineChart(values, ytitle, xtitle) {
+
+	for(i = 0; i < values.length; i++) {
+		values[i] = parseInt(values[i]);
+	}
+
+	values.unshift('');
+
+	var data = google.visualization.arrayToDataTable([
+		values
+	]);
+
+	var options = {
+		width: 1000,
+		height: 563,
+		curveType: 'function',			//smooth the lines
+		hAxis: {title: 'Time'},
+		vAxis: {title: 'Popularity'}
+	};
+
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+
 }
--- WebServer/controller/NodeJsServer/public/js/main.js
+++ WebServer/controller/NodeJsServer/public/js/main.js
@@ -1,10 +1,10 @@
 var templates = {
-  hourly_template: 'chart_template',
-  home_template: 'home_template'
+	hourly_template: 'chart_template',
+	home_template: 'home_template'
 };
 
 var appRouter;
 tpl.loadTemplates([templates.hourly_template, templates.home_template], function () {
-  appRouter = new Router();
-  Backbone.history.start();
+	appRouter = new Router();
+	Backbone.history.start();
 });
