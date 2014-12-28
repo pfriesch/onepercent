7e4afbcb0c536084802157910b4ffdd3e565d0c2
Patrick M <s0540031@htw-berlin.de>
2014-12-28 12:29:06 +0100

    clean up sqlDatabase

--- WebServer/controller/NodeJsServer/sqlDatabase.js
+++ WebServer/controller/NodeJsServer/sqlDatabase.js
@@ -1,5 +1,5 @@
 /**
- * This is the SqlManager to wrtie Data into Database and
+ * This is the SqlManager to write Data into Database and
  * read the sqlData from the Database.
  */
 
@@ -25,48 +25,6 @@ var connectionPool = mysql.createPool (
   }
 );
 
-/* The Layout for the QueryJobs to which table and which fields are needed */
-var sqlQueryLayout = {
-  jobName: 'TopHashtagJob',
-  table: '`toptentags`',
-  params: ['`name`','`timestamp`','`count`']
-};
-
-/*
- * Writes the jobResponseData into Database uses the sqlQueryLayout to get the 
- * right Tablename and Tablefields.
- */
-databaseHandler.writeDataToDatabase = function(responseJobData, jobResult) {
-   connectionPool.getConnection(function(err, connection) {
-
-    for (var i=0; i<responseJobData.jobResult.topHashtags.length; i++) {
-      
-      var sqlInsertQuery = "INSERT INTO "+sqlQueryLayout.table+" ("
-                                         +sqlQueryLayout.params[0]+","
-                                         +sqlQueryLayout.params[1]+","
-                                         +sqlQueryLayout.params[2]+") VALUES ('"
-                                         +responseJobData.jobResult.topHashtags[i].hashtag+"','"
-                                         +jobResult.time+"','"
-                                         +responseJobData.jobResult.topHashtags[i].count+"');"
-
-      console.log(sqlInsertQuery);
-      
-      connection.query(sqlInsertQuery, function(err, rows, fields) {
-        if(err) throw err;
-      });
-    }
-    
-    var sqlCountAllQuery = "INSERT INTO `countalltags` (`count`, `time`) VALUES ('"
-                            +responseJobData.jobResult.countAllHashtags+"','"
-                            +jobResult.time+"');"
-
-    connection.query(sqlCountAllQuery, function(err, rows, fields) {
-        if(err) throw err;
-      });
-  connection.release();
-  });
-}
-
 /**
  * Function to Query the MySQL Database.
  * @param sql Contains the SQL Query with placeholders for variables.
@@ -85,6 +43,12 @@ databaseHandler.select = function(sql,paramters, callback) {
     });
 };
 
+/**
+ * Function to insert data in a MySQL Database.
+ * @param table         the table name
+ * @param columnNames   array of strings that contains the column names of the table
+ * @param data          array of strings that contains the data to insert into the table
+ */
 databaseHandler.insert = function(table, columnNames, data){
     connectionPool.getConnection(function(err, connection) {
         var sql = "INSERT INTO ?? (??) VALUES (?)";
