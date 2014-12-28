45ae4eee8283ac447de7c1e3b70d97c23ebfc2a8
Patrick M <s0540031@htw-berlin.de>
2014-12-28 22:50:32 +0100

    controller reconnects when socket gets disconnected

--- WebServer/sparkClient.js
+++ WebServer/sparkClient.js
@@ -15,31 +15,45 @@ var callbackToTATWebserver; // Callbackfunction to send the responsedata back to
 /* Connects to the Sparkserver with Configuationfile parameters per Websocket*/
 client.connect(config.sparkServerPORT, config.sparkServerHOST, function() {
     console.log('CONNECTED TO: ' + config.sparkServerHOST + ':' + config.sparkServerPORT);
-  });
+});
   
-  client.on('connect', function() { 
-    console.log('Sparkserver connected');
-  });
+client.on('connect', function() {
+  console.log('Sparkserver connected');
+});
 
-  /* Gets the responsedata from Sparkserver and send it back to OP_Webserver by callbackfunction */
-  client.on('data', function(dataResponse) {
-    var dataResponseJson = JSON.parse(dataResponse);
-    if ('errorMessage' in dataResponseJson) {
-      sparkErrorMessageResponse(dataResponseJson);
-    }
-    else {
-      //console.log(dataResponseJson);
-      callbackToTATWebserver(dataResponseJson);
-    }
-  });
+/* Gets the responsedata from Sparkserver and send it back to OP_Webserver by callbackfunction */
+client.on('data', function(dataResponse) {
+  var dataResponseJson = JSON.parse(dataResponse);
+  if ('errorMessage' in dataResponseJson) {
+    sparkErrorMessageResponse(dataResponseJson);
+  }
+  else {
+    //console.log(dataResponseJson);
+    callbackToTATWebserver(dataResponseJson);
+  }
+});
 
-  client.on('error', function(e) {
-    console.log("Error: " + e);
-  });
+client.on('error', function(e) {
+  console.log(new Date() + "Socket Error: " + e);
+  if(e.code == 'ECONNREFUSED') {
+    client.setTimeout(10000, function() {
+      client.connect(config.sparkServerPORT, config.sparkServerHOST, function(){
+        console.log('RECONNECTED TO: ' + config.sparkServerHOST + ':' + config.sparkServerPORT);
+      });
+    });
+    console.log('Timeout for 10 seconds before trying to reconnect again');
+  }
+});
 
-  client.on('close', function() {
-      console.log('Connection closed');
+client.on('close', function() {
+  console.log(new Date() + "Socket Connection closed trying to reconnect.");
+  client.setTimeout(30000, function() {
+    client.connect(config.sparkServerPORT, config.sparkServerHOST, function(){
+      console.log('RECONNECTED TO: ' + config.sparkServerHOST + ':' + config.sparkServerPORT);
+    });
   });
+  console.log('Timeout for 30 seconds before trying to reconnect again');
+});
 
 /* Sends the jobData to sparkserver */
 sparkClient.sendJobDataToServer = function sendJobRequestToSparkServer(jobData, callback) {
