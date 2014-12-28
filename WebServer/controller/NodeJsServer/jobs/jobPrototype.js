aad14f16c42ba448714c75eb0478286f849bb8c5
Patrick M <s0540031@htw-berlin.de>
2014-12-28 16:00:54 +0100

    added wordsearchjob to controller

--- WebServer/controller/NodeJsServer/jobs/jobPrototype.js
+++ WebServer/controller/NodeJsServer/jobs/jobPrototype.js
@@ -3,6 +3,9 @@
  *
  * @author Patrick Mariot
  */
+
+var moment = require('moment'); //Timestampparser
+
 var Job = function(name, table, columnNames) {
     this.name = name;
     this.table = table;
@@ -23,6 +26,26 @@ Job.prototype.getColumnNames = function(){
 
 Job.prototype.saveToDatabase = function(){
 
+};
+
+Job.prototype.createJob = function(jobId, inputParams, offset){
+    var params = inputParams.slice();
+    params.unshift(this.generateTimestamp(offset));
+    return {
+        "jobID": jobId,
+        "name": this.getName(),
+        "params": params,
+        "time": this.generateTimestamp(0)
+    };
+};
+
+/* Generates actual timestamp and adds an offset*/
+Job.prototype.generateTimestamp = function(offset) {
+    offset = typeof offset !== 'undefined' ? offset : 0;
+    var time = moment().add(offset, 'hours');
+    var timeString = time.format('YYYY-MM-DD HH:mm:ss');
+
+    return timeString;
 };
 
 module.exports = Job;
