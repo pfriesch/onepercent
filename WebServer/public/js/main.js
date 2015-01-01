e36de1f4543bd6c5d63cc8f5a4518c743f99db07
Patrick M <s0540031@htw-berlin.de>
2015-01-01 16:40:28 +0100

    include new templates

--- WebServer/public/js/main.js
+++ WebServer/public/js/main.js
@@ -1,10 +1,18 @@
 var templates = {
-	hourly_template: 'chart_template',
-	home_template: 'home_template'
+	hourly_template: 'hourly_navigation_template',
+	home_template: 'home_template',
+	tophashtag_template: 'tophashtag_template',
+	empty_template: 'empty_template'
+};
+
+Backbone.View.prototype.close = function() {
+	console.log("THIS GOT CALLED");
+	this.unbind();
+	this.remove();
 };
 
 var appRouter;
-tpl.loadTemplates([templates.hourly_template, templates.home_template], function () {
+tpl.loadTemplates([templates.hourly_template, templates.home_template, templates.tophashtag_template, templates.empty_template], function () {
 	appRouter = new Router();
 	Backbone.history.start();
 });
