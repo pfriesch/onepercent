e993bc6977161faa230cba37518ca9e3a3f773f9
Patrick M <s0540031@htw-berlin.de>
2015-01-01 18:39:33 +0100

    rework chart.js

--- WebServer/public/js/charts.js
+++ WebServer/public/js/charts.js
@@ -1,9 +1,13 @@
 google.load("visualization", "1", {packages: ["corechart"]});
-//google.setOnLoadCallback(drawChart);
 
 onepercent = {
-/////////////////////////draw columnchart/////////////////////////////////////
-
+    /**
+     * Draws a column chart into the element with the id 'chart'
+     * @param names     Array that contains the column names
+     * @param values    Array that contains the column values
+     * @param ytitle    String that contains the caption of the y-axis
+     * @param xtitle    String that contains the caption of the x-axis
+     */
     drawColumnChart: function (names, values, ytitle, xtitle) {
 
         for (var i = 0; i < values.length; i++) {
@@ -27,115 +31,31 @@ onepercent = {
         chart.draw(data, options);
     },
 
+    /**
+     * Draws a pie chart into the element with the id 'chart'
+     * @param names         Array that contains the column names
+     * @param values        Array that contains the column values
+     * @param chartTitle    String that contains the title of the chart
+     */
+    drawPieChart: function (names, values, chartTitle) {
 
-/////////////////////////draw piechart/////////////////////////////////////
+        var raw_data = new Array();
+        // Needs to be set maybe make it variable
+        raw_data.push(new Array('Label1', 'Label2'));
 
-    drawPieChart: function (names, values) {
-
-        for (i = 0; i < values.length; i++) {
-            values[i] = parseInt(values[i]);
+        for (var i = 0; i < values.length; i++) {
+            raw_data.push(new Array(names[i], parseFloat(values[i])));
         }
 
-        names.unshift('');
-        values.unshift('');
-
-        var data = google.visualization.arrayToDataTable([
-            names,
-            values
-        ]);
-
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
-        chart.draw(data, options);
-    },
-
-
-/////////////////////////draw donutchart/////////////////////////////////////
-
-    drawDonutChart: function (names, values) {
-
-        for (i = 0; i < values.length; i++) {
-            values[i] = parseInt(values[i]);
-        }
-
-        names.unshift('');
-        values.unshift('');
-
-        var data = google.visualization.arrayToDataTable([
-            names,
-            values
-        ]);
+        var data = google.visualization.arrayToDataTable(
+            raw_data
+        );
 
         var options = {
-            pieHole: 0.4,
+            title: chartTitle
         };
 
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
+        var chart = new google.visualization.PieChart(document.getElementById('chart'));
         chart.draw(data, options);
-    },
-
-
-/////////////////////////draw diffcolumnchart/////////////////////////////////////
-
-    drawDiffColumnChart: function (names, diffnames, values, diffvalues, ytitle, xtitle) {
-
-        for (i = 0; i < values.length; i++) {
-            values[i] = parseInt(values[i]);
-        }
-
-        names.unshift('');
-        values.unshift('');
-
-        for (i = 0; i < diffvalues.length; i++) {
-            diffvalues[i] = parseInt(diffvalues[i]);
-        }
-
-        diffnames.unshift('');
-        diffvalues.unshift('');
-
-        var olddata = google.visualization.arrayToDataTable([
-            names,
-            values
-        ]);
-
-        var newdata = google.visualization.arrayToDataTable([
-            diffnames,
-            diffvalues
-        ]);
-
-        var options = {
-            vAxis: {title: ytitle},
-            hAxis: {title: xtitle}
-        };
-
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
-        var diffData = chart.computeDiff(oldData, newData);
-        chart.draw(diffData, options);
-    },
-
-/////////////////////////draw linechart/////////////////////////////////////
-
-    drawLineChart: function (values, ytitle, xtitle) {
-
-        for (i = 0; i < values.length; i++) {
-            values[i] = parseInt(values[i]);
-        }
-
-        values.unshift('');
-
-        var data = google.visualization.arrayToDataTable([
-            values
-        ]);
-
-        var options = {
-            width: 1000,
-            height: 563,
-            curveType: 'function',			//smooth the lines
-            hAxis: {title: 'Time'},
-            vAxis: {title: 'Popularity'}
-        };
-
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
-        chart.draw(data, options);
-
     }
-}
+};
