5198556434c8f45b2167d5f19d9b2f8730155718
Patrick M <s0540031@htw-berlin.de>
2015-01-01 18:47:24 +0100

    added languagecount model to view

---
+++ WebServer/public/js/models/languagecount.js
@@ -0,0 +1,49 @@
+/**
+ *	Model that contains the attributes name and count.
+ * @author Patrick Mariot
+ */
+var NameCountModel = Backbone.Model.extend({
+	url: "api/hourly",
+
+	initialize: function(table, date, hour) {
+		this.url = this.url + "/" + table + "/" + date + "/" + hour;
+	}
+
+});
+
+/**
+ * Collection that contains the NameCountModels.
+ * @author Patrick Mariot
+ */
+var NameCountCollection = Backbone.Collection.extend({
+	model: NameCountModel,
+	url: "api/hourly",
+	
+	initialize: function(table, date, hour){
+		this.url = this.url + "/" + table + "/" + date + "/" + hour;
+	},
+
+	/**
+	 * Creates an array that contain all language Attributes from the models.
+	 * @returns {Array}	of Strings containing all names from the models.
+	 */
+	getLanguages: function(){
+		var languages = new Array();
+		this.each(function(data){
+			languages.push(data.get('language'));
+		}, this);
+		return languages;
+	},
+
+	/**
+	 * Creates an array that contain all count Attributes from the models.
+	 * @returns {Array}	of Strings containing all counts from the models.
+	 */
+	getValues: function() {
+		var values = new Array();
+		this.each(function(data){
+			values.push(data.get('count'));
+		}, this);
+		return values;
+	}
+});
