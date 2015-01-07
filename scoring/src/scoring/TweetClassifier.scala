8033713db3752e0b702d73281c5d75d121511d4c
pFreisch <p.friesch@posteo.de>
2015-01-07 14:41:47 +0100

    normalized output to 0..1

--- scoring/src/scoring/TweetClassifier.scala
+++ scoring/src/scoring/TweetClassifier.scala
@@ -14,10 +14,14 @@ class TweetClassifier(categoryProb: Map[String, Double], termProb: Map[String, M
     val tokenizedTweet = Tokenizer.tokenize(tweet)
     val categories: List[Category] = categoryProb.map(X => X._1).toList
     val score: Map[Category, Double] = categories.map { C =>
-      (C, Math.log10(categoryProb(C) + tokenizedTweet.map(S => termProb.getOrElse(S,Map()).getOrElse(C,0.toDouble)).reduce(_ + _)))
+      (C, Math.log10(categoryProb(C) + tokenizedTweet.map(S => termProb.getOrElse(S, Map()).getOrElse(C, 0.toDouble)).reduce(_ + _)))
     }.toMap
-
-    score
+    //normalizes to 0..1
+    def normalize(classifications: Map[Category, Double]): Map[Category, Double] = {
+      val sum: (Category,Double) = classifications.reduce((X, Y) => (X._1, X._2 + Y._2))
+      classifications.map(X => (X._1, X._2 / sum._2))
+    }
+    normalize(score)
   }
 
 }
