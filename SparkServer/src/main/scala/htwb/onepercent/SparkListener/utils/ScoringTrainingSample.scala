8100e5dc5c5a587ca19cbc787bada63e78e59e78
flowmoe <sir.psycho@gmx.net>
2015-01-10 13:55:00 +0100

    added training sample, local machine probs, build may be fail

---
+++ SparkServer/src/main/scala/htwb/onepercent/SparkListener/utils/ScoringTrainingSample.scala
@@ -0,0 +1,14 @@
+package htwb.onepercent.SparkListener.utils
+
+import scala.collection.immutable.List
+
+/**
+ * Created by flo on 10.01.15.
+ */
+object ScoringTrainingSample {
+
+  def trainingText() : List[String] = {
+    List("default")
+  }
+
+}
