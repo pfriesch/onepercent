f7f8d457410b769075cd49368e85e013f83db960
Patrick M <s0540031@htw-berlin.de>
2015-01-28 21:19:10 +0100

    disable CategoryDistribution Job in Node.js

--- WebServer/OP_Webserver.js
+++ WebServer/OP_Webserver.js
@@ -30,7 +30,7 @@ function initJobInterval(){
 	repeatJobPerInterval('TopHashtagJob', [10], 1000*60*60, -1); //1000*60*60
         repeatJobPerInterval('LanguageDistributionJob', [], 1000*60*60, -1);
         repeatJobPerInterval('OriginTweetsJob', [], 1000*60*60, -1);
-        repeatJobPerInterval('CategoryDistributionJob', [], 1000*60*60, -1);
+        //repeatJobPerInterval('CategoryDistributionJob', [], 1000*60*60, -1);
 	});
 
   wait(moment().endOf('day').add(14,'hours').add(10,'minutes') - moment(), function(){
