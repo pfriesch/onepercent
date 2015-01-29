cdae2d9bc333cd0469a2d3e1fa8750879a130e11
pFreisch <p.friesch@posteo.de>
2015-01-29 19:45:13 +0100

    added Errorhandling to wordSearch

--- WebServer/public/js/views/wordsearch.js
+++ WebServer/public/js/views/wordsearch.js
@@ -3,68 +3,75 @@
  * @author Patrick Mariot
  */
 var WordSearchView = Backbone.View.extend({
-	/**
-	 * Constructor for the view.
-	 * @param options	needs to contain:
-	 * 					- el: 		element where the template gets embedded
-	 * 					- template: the template to use
-	 * 					- table:	the table to show
-	 * 					- searchWord:the word to search for
-	 */
-	initialize: function(options) {
-		_.bindAll(this, 'render', 'changeData', 'showChart');
-		this.setElement(options.el);
-		this.template = _.template(tpl.get(options.template));
-		this.path = {
-			table: options.table,
-			searchWord: encodeURIComponent(options.searchWord)
-		};
+    /**
+     * Constructor for the view.
+     * @param options    needs to contain:
+     *                    - el:        element where the template gets embedded
+     *                    - template: the template to use
+     *                    - table:    the table to show
+     *                    - searchWord:the word to search for
+     */
+    initialize: function (options) {
+        _.bindAll(this, 'render', 'changeData', 'showChart');
+        this.setElement(options.el);
+        this.template = _.template(tpl.get(options.template));
+        this.path = {
+            table: options.table,
+            searchWord: encodeURIComponent(options.searchWord)
+        };
 
-		if(typeof this.path.searchWord != 'undefined'){
-			this.dataCollection = new WordSearchCollection(this.path.table, this.path.searchWord);
-			this.dataCollection.fetch({reset: true});
-			this.dataCollection.on('sync', this.showChart);
-		}
+        if (typeof this.path.searchWord != 'undefined') {
+            this.dataCollection = new WordSearchCollection(this.path.table, this.path.searchWord);
+            this.dataCollection.fetch({
+                success: function (c, r, o) {
+                }, error: function (c, r, o) {
+                    alert("No data available, try again!");
+                }
+            });
+            this.dataCollection.on('sync', this.showChart);
+        }
 
-		this.render();
-	},
-	
-	render: function() {
-		this.$el.html(this.template());
-	},
+        this.render();
+    },
 
-	/**
-	 * Updates the data for the view.
-	 * @param table	the table to get the data from
-	 * @param searchWord	the word to look for
-	 */
-	changeData: function(table, searchWord){
-		console.log(searchWord);
-		this.path = {
-			table: table,
-			searchWord: encodeURIComponent(searchWord)
-		};
+    render: function () {
+        this.$el.html(this.template());
+    },
 
-		if(typeof this.dataCollection != 'undefined'){
-			this.dataCollection.remove();
-		}
+    /**
+     * Updates the data for the view.
+     * @param table    the table to get the data from
+     * @param searchWord    the word to look for
+     */
+    changeData: function (table, searchWord) {
+        console.log(searchWord);
+        this.path = {
+            table: table,
+            searchWord: encodeURIComponent(searchWord)
+        };
 
-		console.log(this.path.searchWord);
+        if (typeof this.dataCollection != 'undefined') {
+            this.dataCollection.remove();
+        }
 
-		this.dataCollection = new WordSearchCollection(this.path.table, this.path.searchWord);
-		this.dataCollection.fetch({reset: true});
-		this.dataCollection.on('sync', this.showChart);
-	},
+        console.log(this.path.searchWord);
 
-	/**
-	 * Prepares the data for the chart.
-	 */
-	showChart: function(){
-		var timestamps = this.dataCollection.getTimestamps()
-		var values = this.dataCollection.getValues();
-		var word = this.dataCollection.getUniqNames()[0];
+        this.dataCollection = new WordSearchCollection(this.path.table, this.path.searchWord);
+        this.dataCollection.fetch({reset: true});
+        this.dataCollection.on('sync', this.showChart);
+    },
 
-		this.render();
-		onepercent.drawLineChart(timestamps, values, word, 'Count');
-	}
+    /**
+     * Prepares the data for the chart.
+     */
+    showChart: function () {
+        console.log("test2")
+
+        var timestamps = this.dataCollection.getTimestamps()
+        var values = this.dataCollection.getValues();
+        var word = this.dataCollection.getUniqNames()[0];
+
+        this.render();
+        onepercent.drawLineChart(timestamps, values, word, 'Count');
+    }
 });
--- WebServer/restfulapi.js
+++ WebServer/restfulapi.js
@@ -20,31 +20,32 @@ var app = express(); //create a new expressObject
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(config.browserClientPort, function(){
-  dataLogger.logData("Server is up and running");
+
+var server = app.listen(config.browserClientPort, function () {
+    dataLogger.logData("Server is up and running");
 });
 
 /*
  * If the browserClient requests with URLparams (/api/:table)
  * this methode gets the needed data from database and responds it to the Browserclient.
  */
-app.get('/api/:table', function(req, res){
-  dataBaseHandler.select("SELECT timestamp FROM ?? GROUP BY timestamp", [req.params.table], function(result){
-    res.send(result);
-  });
+app.get('/api/:table', function (req, res) {
+    dataBaseHandler.select("SELECT timestamp FROM ?? GROUP BY timestamp", [req.params.table], function (result) {
+        res.send(result);
+    });
 });
 
 /*
  * If the browserClient requests with URLparams (/api/:table)
  * this methode gets the needed data from database and responds it to the Browserclient.
  */
-app.get('/api/live/:table', function(req, res){
-  var time = moment();
-  var date = createSQLDate(time.format('YYYY-MM-DD'), time.format('HH'), -48);
-  var nextDate = createSQLDate(time.format('YYYY-MM-DD'), time.format('HH'), +1);
-  dataBaseHandler.select("SELECT name FROM ?? WHERE written >= ? AND written < ?", [req.params.table,date,nextDate], function(result){
-    res.send(result);
-  });
+app.get('/api/live/:table', function (req, res) {
+    var time = moment();
+    var date = createSQLDate(time.format('YYYY-MM-DD'), time.format('HH'), -48);
+    var nextDate = createSQLDate(time.format('YYYY-MM-DD'), time.format('HH'), +1);
+    dataBaseHandler.select("SELECT name FROM ?? WHERE written >= ? AND written < ?", [req.params.table, date, nextDate], function (result) {
+        res.send(result);
+    });
 });
 
 /*
@@ -52,69 +53,68 @@ app.get('/api/live/:table', function(req, res){
  * this methode gets the needed data from database and responds it to the Browserclient.
  * returns available data, if the the word was found, or null if the word wasn´t found or it is not valid
  */
-app.get('/api/live/wordsearch/:searchWord', function wordSearchREST(req, res){
-  res.send(null);
-  /*
-  var time = moment();
-  var date = createSQLDate(time.format('YYYY-MM-DD'), time.format('HH'), -48);
-  var nextDate = createSQLDate(time.format('YYYY-MM-DD'), time.format('HH'), +1);
-  var searchWord = decodeURIComponent(req.params.searchWord);
-  dataBaseHandler.select("SELECT * FROM ?? WHERE written >= ? AND written < ? AND name = ?", ['wordsearch',date,nextDate, searchWord], function(result){
-    if(result.length > 0){
-      res.send(result);
-    } else if (req.params.secondTry != true) {
-      try {
-        var wordSearchJob = jobManager.createJob('WordSearchJob', [searchWord]);
-        sparkClient.sendJobDataToServer(wordSearchJob, function (dataResponse) {
-          var jobType = jobManager.getJobTypeByName(wordSearchJob.name);
-          jobType.saveToDatabase(dataResponse, wordSearchJob, function () {
-            req.params.secondTry = true;
-            wordSearchREST(req, res, true);
-          });
-        });
-      } catch(ex){
-        dataLogger.logData(ex);
-        res.send(null);
-      }
-    } else {
-      res.send(null);
-    }
-  });*/
+app.get('/api/live/wordsearch/:searchWord', function wordSearchREST(req, res) {
+    var time = moment();
+    var date = createSQLDate(time.format('YYYY-MM-DD'), time.format('HH'), -48);
+    var nextDate = createSQLDate(time.format('YYYY-MM-DD'), time.format('HH'), +1);
+    var searchWord = decodeURIComponent(req.params.searchWord);
+    dataBaseHandler.select("SELECT * FROM ?? WHERE written >= ? AND written < ? AND name = ?", ['wordsearch', date, nextDate, searchWord], function (result) {
+        if (result.length > 0) {
+            res.send(result);
+        } else if (req.params.secondTry != true) {
+            try {
+                var wordSearchJob = jobManager.createJob('WordSearchJob', [searchWord]);
+                sparkClient.sendJobDataToServer(wordSearchJob, function (dataResponse) {
+                    var jobType = jobManager.getJobTypeByName(wordSearchJob.name);
+                    jobType.saveToDatabase(dataResponse, wordSearchJob, function () {
+                        req.params.secondTry = true;
+                        wordSearchREST(req, res, true);
+                    });
+                });
+            } catch (ex) {
+                dataLogger.logData(ex);
+                res.send("Job analysis failed");
+            }
+        } else {
+            //res.send({"errorMsg": "Cant find Job", "code": 404});
+            res.send("Cant find Job");
+        }
+    });
 });
 
 /*
  * If the browserClient requests with URLparams (/api/daily/:table/:date)
  * this methode gets the needed data from database and responds it to the Browserclient.
  */
-app.get('/api/daily/:table/:date', function(req, res){
-  var date = createSQLDate(req.params.date, 00);
-  var nextDate = createSQLDate(req.params.date, 00, +24);
-  dataBaseHandler.select("SELECT * FROM ?? WHERE timestamp >= ? AND timestamp < ?", [req.params.table,date,nextDate], function(result){
-    res.send(result);
-  });
+app.get('/api/daily/:table/:date', function (req, res) {
+    var date = createSQLDate(req.params.date, 00);
+    var nextDate = createSQLDate(req.params.date, 00, +24);
+    dataBaseHandler.select("SELECT * FROM ?? WHERE timestamp >= ? AND timestamp < ?", [req.params.table, date, nextDate], function (result) {
+        res.send(result);
+    });
 });
 
 /*
  * If the browserClient requests with URLparams (/api/hourly/:table/:date/:hour) 
  * this methode gets the needed data from database and responds it to the Browserclient.
  */
-app.get('/api/hourly/:table/:date/:hour', function(req, res){
-  var date = createSQLDate(req.params.date, req.params.hour);
-  var nextDate = createSQLDate(req.params.date, req.params.hour, +1);
-  dataBaseHandler.select("SELECT * FROM ?? WHERE timestamp >= ? AND timestamp < ? ORDER BY count DESC", [req.params.table,date,nextDate], function(result){
-    res.send(result);
-  });
+app.get('/api/hourly/:table/:date/:hour', function (req, res) {
+    var date = createSQLDate(req.params.date, req.params.hour);
+    var nextDate = createSQLDate(req.params.date, req.params.hour, +1);
+    dataBaseHandler.select("SELECT * FROM ?? WHERE timestamp >= ? AND timestamp < ? ORDER BY count DESC", [req.params.table, date, nextDate], function (result) {
+        res.send(result);
+    });
 });
 
 /*
  * Creates a sql timestamp to write the data to database.
  */
-function createSQLDate(date, hour, offset){
-  hour = typeof hour !== 'undefined' ? hour : 0;
-  offset = typeof offset !== 'undefined' ? offset : 0;
+function createSQLDate(date, hour, offset) {
+    hour = typeof hour !== 'undefined' ? hour : 0;
+    offset = typeof offset !== 'undefined' ? offset : 0;
 
-  var time = moment(date);
-  time.hour(hour);
-  time.add(offset, 'hours');
-  return time.format('YYYY-MM-DD HH:mm:ss');
+    var time = moment(date);
+    time.hour(hour);
+    time.add(offset, 'hours');
+    return time.format('YYYY-MM-DD HH:mm:ss');
 }
