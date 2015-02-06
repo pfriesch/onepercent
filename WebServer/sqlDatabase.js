81a307ab31c8ad21927c25d25699d755ed9a6328
pFreisch <p.friesch@posteo.de>
2015-02-06 14:23:03 +0100

    added error handling for spark and db connection in manager

--- WebServer/sqlDatabase.js
+++ WebServer/sqlDatabase.js
@@ -8,21 +8,22 @@
 var mysql = require('mysql'); // MySQL
 var moment = require('moment'); //Timestampparser
 var config = require('./config.js'); //Configurationfile
+var dataLogger = require('./helper.js'); // helperfunctions
 
 var databaseHandler = exports; //exports the Database classes
 
 /* Creates a connectionpool to the Database*/
-var connectionPool = mysql.createPool (
-  {
-    host     : config.sqlDatabaseHost,
-    port     : config.sqlDatabasePort,
-    user     : config.sqlDatabaseUser,
-    password : config.sqlDatabasePassword,
-    database : config.sqlDatabase,
-    timezone : config.sqlDatabaseTimezone,
-    charset  : config.sqlDatabaseCharset,
-    connectionLimit: config.sqlConnectionLimit
-  }
+var connectionPool = mysql.createPool(
+    {
+        host: config.sqlDatabaseHost,
+        port: config.sqlDatabasePort,
+        user: config.sqlDatabaseUser,
+        password: config.sqlDatabasePassword,
+        database: config.sqlDatabase,
+        timezone: config.sqlDatabaseTimezone,
+        charset: config.sqlDatabaseCharset,
+        connectionLimit: config.sqlConnectionLimit
+    }
 );
 
 /**
@@ -31,16 +32,24 @@ var connectionPool = mysql.createPool (
  * @param paramters Array that contains the variables.
  * @param callback Function to call when the result is avaible.
  */
-databaseHandler.select = function(sql, paramters, callback) {
-    connectionPool.getConnection(function (err, connection) {
-        connection.query(sql, paramters, function(err, rows) {
-            if(err) {
-               console.log(err);
+databaseHandler.select = function (sql, paramters, callback) {
+    try {
+        connectionPool.getConnection(function (err, connection) {
+            if (connection == undefined) {
+                dataLogger.logData("[ERROR] Can't connect to Database, check your credentials");
+            } else {
+                connection.query(sql, paramters, function (err, rows) {
+                    if (err) {
+                        dataLogger.logData(err);
+                    }
+                    connection.release();
+                    callback(rows);
+                });
             }
-            connection.release();
-            callback(rows);
         });
-    });
+    } catch (ex) {
+        dataLogger.logData("[ERROR] Can't connect to Database, check your credentials");
+    }
 };
 
 /**
@@ -49,15 +58,22 @@ databaseHandler.select = function(sql, paramters, callback) {
  * @param columnNames   array of strings that contains the column names of the table
  * @param data          array of strings that contains the data to insert into the table
  */
-databaseHandler.insert = function(table, columnNames, data){
-    connectionPool.getConnection(function(err, connection) {
-        
-        var sql = "INSERT INTO ?? (??) VALUES (?)";         
-        connection.query(sql, [table, columnNames, data], function (err, rows) {
-            if(err){
-                console.log(err);
+databaseHandler.insert = function (table, columnNames, data) {
+    try {
+        connectionPool.getConnection(function (err, connection) {
+            if (connection == undefined) {
+                dataLogger.logData("[ERROR] Can't connect to Database, check your credentials");
+            } else {
+                var sql = "INSERT INTO ?? (??) VALUES (?)";
+                connection.query(sql, [table, columnNames, data], function (err, rows) {
+                    if (err) {
+                        dataLogger.logData(err);
+                    }
+                    connection.release();
+                });
             }
-            connection.release();
         });
-    });
+    } catch (ex) {
+        dataLogger.logData("[ERROR] Can't connect to Database, check your credentials");
+    }
 };
