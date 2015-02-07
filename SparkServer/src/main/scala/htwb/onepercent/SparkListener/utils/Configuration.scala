f3a89abe51341a0c7b4f1fe493b9795a6cb71078
pFreisch <p.friesch@posteo.de>
2015-02-07 21:36:00 +0100

    changed default threshold for a good classified Tweet

--- SparkServer/src/main/scala/htwb/onepercent/SparkListener/utils/Configuration.scala
+++ SparkServer/src/main/scala/htwb/onepercent/SparkListener/utils/Configuration.scala
@@ -59,7 +59,7 @@ object Config extends Serializable {
   val defaultClassificationOtherCategoryName: String = "other"
   //SmallTrainingSet 0.16
   val defaultClassificationThreshold: Double = 0.16
-  val defaultScoringMinProbForResult: Double = 0.9
+  val defaultScoringMinProbForResult: Double = 0.95
   val defaultConfig = Configuration(configVersion,
     defaultHostname,
     defaultPort,
