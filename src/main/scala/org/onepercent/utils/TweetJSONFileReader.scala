f6598644a6b575acc48df455321b9132a9aa5111
flowmoe <sir.psycho@gmx.net>
2015-02-22 18:53:55 +0100

    changed documentation to english

--- src/main/scala/org/onepercent/utils/TweetJSONFileReader.scala
+++ src/main/scala/org/onepercent/utils/TweetJSONFileReader.scala
@@ -10,38 +10,38 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.hive._
 
 /**
-* JSONFileReader to read JSON Files and extract data.
-*
-* Wegen des HiveContext ist mindestens Apache Spark 1.1.0 erforderlich.
-* Quellen:
-* - http://blog.cloudera.com/blog/2012/11/analyzing-twitter-data-with-hadoop-part-3-querying-semi-structured-data-with-hive/
-* - http://apache-spark-user-list.1001560.n3.nabble.com/flattening-a-list-in-spark-sql-td13300.html
-* - http://apache-spark-user-list.1001560.n3.nabble.com/Query-the-nested-JSON-data-With-Spark-SQL-1-0-1-td9544.html
-*
-* @author Patrick Mariot, Florian Willich
-**/
+ * JSONFileReader to read JSON Files and extract data.
+ *
+ * Due to the us of the HiveContext there is the need for Apache Spark 1.1.0 (or higher):.
+ * Sources:
+ * - http://blog.cloudera.com/blog/2012/11/analyzing-twitter-data-with-hadoop-part-3-querying-semi-structured-data-with-hive/
+ * - http://apache-spark-user-list.1001560.n3.nabble.com/flattening-a-list-in-spark-sql-td13300.html
+ * - http://apache-spark-user-list.1001560.n3.nabble.com/Query-the-nested-JSON-data-With-Spark-SQL-1-0-1-td9544.html
+ *
+ * @author Patrick Mariot, Florian Willich
+ **/
 class TweetJSONFileReader(sc: SparkContext, hiveContext: HiveContext) {
 
-	/**
-	* Reads the JSON File and returns a SchemaRDD.
-	**/
-	def readFile(pathToJSONFile: String) : SchemaRDD = {
-		hiveContext.jsonFile(pathToJSONFile)
-	}
+  /**
+   * Reads the JSON File and returns a SchemaRDD.
+   **/
+  def readFile(pathToJSONFile: String) : SchemaRDD = {
+    hiveContext.jsonFile(pathToJSONFile)
+  }
 
-	/**
-	 * Read a List of T_Path and returns the result in one SchemaRDD.
-	 * @param pathListToJSONFile List of validate Paths
-	 * @return SchemaRDD that contains Data from all Paths
-	 */
-	def readFile(pathListToJSONFile: List[Path]) : SchemaRDD = {
-		var result: SchemaRDD = hiveContext.jsonFile(pathListToJSONFile.head.path)
-		// setting Schema at hiveContext.jsonFile improves performance by a lot
-		val dataSchema: StructType = result.schema
-		for(i <- 1 to (pathListToJSONFile.length - 1)) {
-			result = result.unionAll(hiveContext.jsonFile(pathListToJSONFile(i).path, dataSchema))
-		}
-		result
-	}
+  /**
+   * Read a List of T_Path and returns the result in one SchemaRDD.
+   * @param pathListToJSONFile List of validate Paths
+   * @return SchemaRDD that contains Data from all Paths
+   */
+  def readFile(pathListToJSONFile: List[Path]) : SchemaRDD = {
+    var result: SchemaRDD = hiveContext.jsonFile(pathListToJSONFile.head.path)
+    // setting Schema at hiveContext.jsonFile improves performance by a lot
+    val dataSchema: StructType = result.schema
+    for(i <- 1 to (pathListToJSONFile.length - 1)) {
+      result = result.unionAll(hiveContext.jsonFile(pathListToJSONFile(i).path, dataSchema))
+    }
+    result
+  }
 
 }
